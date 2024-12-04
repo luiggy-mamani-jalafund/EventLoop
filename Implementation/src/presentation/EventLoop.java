88e5b5fe40b8bb0a5b16ed7763bece7af08a0479
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-12-03 23:55:40 -0400

    feat: event loop adapted to new event loop handler

--- Implementation/src/presentation/EventLoop.java
+++ Implementation/src/presentation/EventLoop.java
@@ -5,23 +5,48 @@ import domain.entities.tasks.interfaces.ITask;
 import infrastructure.useCases.EventLoopHandler;
 import infrastructure.useCases.queues.CallStackHandler;
 
-public class EventLoop {
+public class EventLoop implements AutoCloseable {
     private final EventLoopHandler eventLoopHandler;
+    private final Thread eventLoopThread;
 
     public EventLoop() {
         ICallstackHandler callstackHandler = new CallStackHandler();
         this.eventLoopHandler = new EventLoopHandler(callstackHandler);
+        this.eventLoopThread = new Thread(this::runEventLoop, "EventLoop-Thread");
+    }
+
+    public void start() {
+        if (!isRunning()) {
+            throw new IllegalStateException("Cannot start a shutdown event loop");
+        }
+        eventLoopThread.start();
     }
 
     public void execute(ITask<Runnable> task) {
         eventLoopHandler.executeTask(task);
     }
 
-    public void run() {
+    private void runEventLoop() {
         try {
             eventLoopHandler.run();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void shutdown() {
+        eventLoopHandler.shutDown();
+        eventLoopThread.interrupt();
+    }
+
+    public boolean isRunning() {
+        return eventLoopHandler.isRunning();
+    }
+
+    @Override
+    public void close() {
+        shutdown();
     }
 }
