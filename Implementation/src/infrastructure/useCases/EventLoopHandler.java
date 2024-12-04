dbb35aa74547a7104a60146b0f35cfd4cf15e954
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-12-03 23:55:17 -0400

    feat: event loop handler adapter to new interface declaration

--- Implementation/src/infrastructure/useCases/EventLoopHandler.java
+++ Implementation/src/infrastructure/useCases/EventLoopHandler.java
@@ -6,23 +6,48 @@ import domain.entities.tasks.interfaces.ITask;
 import infrastructure.utils.Sleeper;
 
 public class EventLoopHandler implements IEventLoopHandler {
-    private final long SLEEP_LOOP_MILLISECONDS = 1;
+    private static final long SLEEP_LOOP_MILLISECONDS = 1;
     private final ICallstackHandler callstackHandler;
+    private volatile boolean isRunning;
 
     public EventLoopHandler(ICallstackHandler callstackHandler) {
         this.callstackHandler = callstackHandler;
+        this.isRunning = true;
     }
 
     @Override
-    public void run() {
-        while (callstackHandler.hasTasks()) {
-            callstackHandler.runTasks();
-            Sleeper.tryToSleepOrDie(SLEEP_LOOP_MILLISECONDS);
+    public void run() throws InterruptedException {
+        while (isRunning) {
+            if (callstackHandler.hasTasks()) {
+                callstackHandler.runTasks();
+            } else {
+                Sleeper.tryToSleepOrDie(SLEEP_LOOP_MILLISECONDS);
+            }
+
+            if (Thread.interrupted()) {
+                throw new InterruptedException("Event loop was interrupted");
+            }
         }
     }
 
     @Override
     public void executeTask(ITask<Runnable> task) {
+        if (!isRunning) {
+            throw new IllegalStateException("Event loop is shutdown");
+        }
+        if (task == null) {
+            throw new IllegalArgumentException("Task cannot be null");
+        }
         callstackHandler.addTask(task.getExecutor());
+    }
+
+    @Override
+    public void shutDown() {
+        isRunning = false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return isRunning;
     }
 }
