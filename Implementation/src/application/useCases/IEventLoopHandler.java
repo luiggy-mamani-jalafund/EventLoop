74521399b96e9c3d425d6229621ac94206cc402c
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-12-03 23:54:42 -0400

    feat: interface declaration to have more control for event loop

--- Implementation/src/application/useCases/IEventLoopHandler.java
+++ Implementation/src/application/useCases/IEventLoopHandler.java
@@ -3,6 +3,26 @@ package application.useCases;
 import domain.entities.tasks.interfaces.ITask;
 
 public interface IEventLoopHandler {
+    /**
+     * Starts the event loop
+     * @throws InterruptedException if the thread is interrupted
+     */
     void run() throws InterruptedException;
+
+    /**
+     * Adds a task to be executed by the event loop
+     * @param task the task to execute
+     */
     void executeTask(ITask<Runnable> task);
+
+    /**
+     * Gracefully shuts down the event loop
+     */
+    void shutDown();
+
+    /**
+     * Checks if the event loop is currently running
+     * @return true if the event loop is running
+     */
+    boolean isRunning();
 }
