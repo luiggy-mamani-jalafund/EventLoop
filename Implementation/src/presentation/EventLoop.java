fcc6f915a3f1ad1df82cdeae2a5053ae908f723d
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-12-04 20:44:51 -0400

    feat: add logic to add a setTimeout also to cancel a task

--- Implementation/src/presentation/EventLoop.java
+++ Implementation/src/presentation/EventLoop.java
@@ -5,18 +5,21 @@ import application.useCases.queues.IMicrotaskHandler;
 import domain.entities.tasks.concrete.promises.Promise;
 import domain.entities.tasks.interfaces.IPromiseTask;
 import domain.entities.tasks.interfaces.ITask;
+import domain.entities.tasks.interfaces.ITimerTask;
 import infrastructure.useCases.EventLoopHandler;
 import infrastructure.useCases.queues.CallStackHandler;
 import infrastructure.useCases.queues.MicrotaskHandler;
+import infrastructure.useCases.queues.TimerTaskHandler;
 
 public class EventLoop {
     private final EventLoopHandler eventLoopHandler;
 
     public EventLoop() {
-        ICallstackHandler callstackHandler = new CallStackHandler();
-        IMicrotaskHandler microtaskHandler = new MicrotaskHandler();
+        CallStackHandler callstackHandler = new CallStackHandler();
+        MicrotaskHandler microtaskHandler = new MicrotaskHandler();
+        TimerTaskHandler timerTaskHandler = new TimerTaskHandler();
 
-        eventLoopHandler = new EventLoopHandler(callstackHandler, microtaskHandler);
+        this.eventLoopHandler = new EventLoopHandler(callstackHandler, microtaskHandler, timerTaskHandler);
     }
 
     public void execute(ITask<Runnable> task) {
@@ -25,6 +28,14 @@ public class EventLoop {
 
     public <T> Promise<T> execute(IPromiseTask<T> task) {
         return eventLoopHandler.executePromise(task);
+    }
+
+    public void setTimeout(ITimerTask timerTask) {
+        eventLoopHandler.setTimeout(timerTask);
+    }
+
+    public boolean cancelTimeout(String timerTaskId) {
+        return eventLoopHandler.clearTimeout(timerTaskId);
     }
 
     public <T> Promise<T> resolve(T value) {
