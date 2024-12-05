c43d75624a0a7434efd47a057c9bc4345d703e53
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-12-04 21:05:05 -0400

    test: add nested setTimeout tests

---
+++ Implementation/src/tests/SetTimeout/NestedTimeoutTest.java
@@ -0,0 +1,34 @@
+package tests.SetTimeout;
+
+import domain.entities.tasks.concrete.immediates.ImmediateTask;
+import domain.entities.tasks.concrete.timers.TimerTask;
+import presentation.EventLoop;
+
+public class NestedTimeoutTest {
+
+    public static void main(String[] args) {
+        EventLoop eventLoop = new EventLoop();
+
+        eventLoop.execute(new ImmediateTask(() -> System.out.println("Start")));
+
+        eventLoop.setTimeout(new TimerTask(() -> {
+            System.out.println("First message");
+
+            eventLoop.setTimeout(new TimerTask(() -> {
+                System.out.println("Second message");
+
+                eventLoop.setTimeout(new TimerTask(() -> {
+                    System.out.println("Third message");
+                }, 1000));
+            }, 2000));
+        }, 3000));
+
+        eventLoop.run();
+
+        // Console output:
+        // Start
+        // First message
+        // Second message
+        // Third message
+    }
+}
