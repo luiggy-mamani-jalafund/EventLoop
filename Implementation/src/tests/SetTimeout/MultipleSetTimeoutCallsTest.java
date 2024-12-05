6ffedd07e380211adbe50033960647b044a47343
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-12-04 20:56:59 -0400

    test: add test with multiple calls to setTimeout

---
+++ Implementation/src/tests/SetTimeout/MultipleSetTimeoutCallsTest.java
@@ -0,0 +1,47 @@
+package tests.SetTimeout;
+
+import domain.entities.tasks.concrete.immediates.ImmediateTask;
+import domain.entities.tasks.concrete.timers.TimerTask;
+import presentation.EventLoop;
+
+public class MultipleSetTimeoutCallsTest {
+
+    public static void main(String[] args) {
+        EventLoop eventLoop = new EventLoop();
+
+        eventLoop.execute(new ImmediateTask(() -> System.out.println("Start of the program")));
+
+        eventLoop.execute(new ImmediateTask(() -> {
+            System.out.println("First function");
+
+            eventLoop.setTimeout(new TimerTask(() -> {
+                System.out.println("Timeout 1");
+
+                eventLoop.execute(new ImmediateTask(() -> {
+                    System.out.println("Second function");
+
+                    eventLoop.setTimeout(new TimerTask(() -> {
+                        System.out.println("Timeout 2");
+                    }, 0));
+
+                    eventLoop.execute(new ImmediateTask(() -> {
+                        System.out.println("Third function");
+                    }));
+                }));
+            }, 0));
+        }));
+
+        eventLoop.execute(new ImmediateTask(() -> System.out.println("End of the program")));
+
+        eventLoop.run();
+        
+        // Console output:
+        // Start of the program
+        // First function
+        // End of the program
+        // Timeout 1
+        // Second function
+        // Third function
+        // Timeout 2
+    }
+}
