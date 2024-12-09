635da720b32814c1e8d90e296f298085211cdad7
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-12-08 20:25:46 -0400

    feat: add the Event Manager

---
+++ Client/src/main/java/org/example/fibonacci/ReactionGameEventLoop/EventManager.java
@@ -0,0 +1,21 @@
+package org.example.fibonacci.ReactionGameEventLoop;
+
+import domain.entities.tasks.concrete.immediates.ImmediateTask;
+import domain.entities.tasks.concrete.timers.TimerTask;
+import presentation.EventLoop;
+
+public class EventManager {
+    private final EventLoop eventLoop;
+
+    public EventManager(EventLoop eventLoop) {
+        this.eventLoop = eventLoop;
+    }
+
+    public void executeImmediateTask(Runnable task) {
+        eventLoop.execute(new ImmediateTask(task));
+    }
+
+    public void setTimeout(Runnable task, int delay) {
+        eventLoop.setTimeout(new TimerTask(task, delay));
+    }
+}
